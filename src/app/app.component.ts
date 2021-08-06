307b7616e88fb4876f6f7b70029a99d2383c2aee
Jacob Craig <jacob.craig@graduationalliance.com>
2021-08-06 15:41:57 -0600

    adding spacetime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import spacetime from 'spacetime';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-app';
+
+  spaceNow = spacetime.now();
 }
